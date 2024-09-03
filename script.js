995ea9d6fe6a084c77a0dce073fb06ad192abb38
Subhan Ahmed Bhatti <subhanrajput337@gmail.com>
2024-09-03 17:00:01 +0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// script.js
+
+
+
+function showSection(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll('.menu-item');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    // Show the selected section
+    const selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.classList.add('active');
+    }
+}
+
+// Initially show the first section
+document.addEventListener('DOMContentLoaded', function() {
+    showSection('chicken');
+});
 
